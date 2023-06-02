9f03dbfa0a44bec360c33609b60f8aae05f70514
Thiago Da Silva Souza <thiago.silva@grupomfm.com.br>
2023-06-02 15:48:27 -0300

    site

---
+++ js/meu-script.js
@@ -0,0 +1,11 @@
+// Função para mostrar o dropdown de categorias do NavMenu
+
+var dropdownMenu = document.getElementById('dropdownMenu');
+
+function showDropdown() {
+    dropdownMenu.classList.add('show');
+}
+
+function hideDropdown() {
+    dropdownMenu.classList.remove('show');
+}
