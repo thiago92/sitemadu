6e75f312d3e73dfa623224b377e5bfe4e9e9f91a
thiago92 <thiago.dsouza1992@gmail.com>
2023-06-16 12:08:34 -0300

    homeNova

--- js/meu-script.js
+++ js/meu-script.js
@@ -11,5 +11,30 @@ function hideDropdown() {
 }
 
 
-// Função para exibição do navResponsivo
+//Botão no navbar no modo responsivo
+document.getElementById('toggle-button').addEventListener('click', function() {
+    var icon = document.getElementById('toggle-icon');
+  
+    if (icon.classList.contains('fa-bars')) {
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-times');
+    } else {
+      icon.classList.remove('fa-times');
+      icon.classList.add('fa-bars');
+    }
+  });
+
+//navMenu fixada do topo
+window.addEventListener("scroll", function() {
+  var navMenu = document.querySelector(".navMenu");
+  var navTopo = document.querySelector(".navTopo");
+
+  if (window.scrollY > 0) {
+      navMenu.classList.add("fixed-top");
+      navTopo.style.marginBottom = navMenu.offsetHeight + "px";
+  } else {
+      navMenu.classList.remove("fixed-top");
+      navTopo.style.marginBottom = "0";
+    }
+});
 
